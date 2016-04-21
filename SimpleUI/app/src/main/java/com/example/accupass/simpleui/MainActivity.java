478fda4c2ef098607570dae25e042cf2b85326a2
disneyhuang703 <jared0703@gmail.com>
2016-04-21 20:54:44 +0800

    implement text input and add radio button group

--- SimpleUI/app/src/main/java/com/example/accupass/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/accupass/simpleui/MainActivity.java
@@ -2,28 +2,70 @@ package com.example.accupass.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     TextView textView;
+    EditText editText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.textView);
+        textView = (TextView)findViewById(R.id.textView);
         //用(TextView)去把較高的級別轉換為較低的級別
-        textView.setText("Hello World");
+
+        editText = (EditText)findViewById(R.id.editText);
+
+       //按下button後希望把text置換成textField輸入的文字
+
+
+        //setOnkeyListner是一個interface,呼叫它時，就會自動帶出很多method來
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    click(v);
+                    return true;
+                }
+
+
+                return false;
+            }
+        });
+
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE){
+                    click(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
     }
 
 
     public void click(View view)
     {
 
-        textView.setText("Android Class");
+        String text = editText.getText().toString();
+        textView.setText(text);
+        editText.setText("");
 
     }
 
